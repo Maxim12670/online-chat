d7e885cdc5cc907b2cc6d17dff409d188fd30e87
Максим <akaikinmax@mail.ru>
2024-04-09 10:42:12 +0700

    feat / реализация карточки человека

--- client/src/widgets/searchPage/component/searchItem/SearchItem.vue
+++ client/src/widgets/searchPage/component/searchItem/SearchItem.vue
@@ -5,16 +5,16 @@
       <img src="@/shared/assets/image/pict.jpg" alt="avatar">
     </div>
     <div class="search-item__initials">
-      Имя Фамилия
+      {{ `${surname} ${name}` }}
     </div>
     <div class="search-item__btns">
       <button class="search-item__btn">
-        <svg class="nav-menu__icon">
+        <svg class="search-item__icon">
           <use xlink:href="#message-icon"></use>
         </svg>
       </button>
       <button class="search-item__btn">
-        <svg class="nav-menu__icon">
+        <svg class="search-item__icon">
           <use xlink:href="#friends-icon"></use>
         </svg>
       </button>
@@ -25,6 +25,17 @@
 <script setup>
 import './style.scss';
 import { SpriteSVG } from '@/shared/ui/index';
+import { useUserStore } from '@/stores/userStore';
 
+const userStore = useUserStore();
+
+defineProps({
+  id: Number,
+  name: String,
+  surname: String
+})
+
+//добавить функционал - добавить в друзья
+//добавить функиционал - написать сообщение
 
 </script>
