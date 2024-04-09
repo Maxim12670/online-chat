9a6cab26c60fab660a5b0491efc17d74cba6b9ab
Максим <akaikinmax@mail.ru>
2024-04-09 10:01:58 +0700

    fix / убрал файл стилей из спрайта

--- client/src/shared/ui/spriteSVG/SpriteSVG.vue
+++ client/src/shared/ui/spriteSVG/SpriteSVG.vue
@@ -5,7 +5,7 @@
       <g id="SVGRepo_iconCarrier">
         <path
           d="M8 10.5H16M8 14.5H11M21.0039 12C21.0039 16.9706 16.9745 21 12.0039 21C9.9675 21 3.00463 21 3.00463 21C3.00463 21 4.56382 17.2561 3.93982 16.0008C3.34076 14.7956 3.00391 13.4372 3.00391 12C3.00391 7.02944 7.03334 3 12.0039 3C16.9745 3 21.0039 7.02944 21.0039 12Z"
-          stroke="#000000" stroke-width="0.744" stroke-linecap="round" stroke-linejoin="round" />
+           stroke-width="0.744" stroke-linecap="round" stroke-linejoin="round" />
       </g>
 
     </symbol>
@@ -14,11 +14,11 @@
       <g>
         <path
           d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
-          stroke="#000000" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+           stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
       </g>
     </symbol>
 
-    <symbol id="friends-icon" viewBox="0 0 16.00 16.00" fill="none" stroke="#000000" stroke-linecap="round"
+    <symbol id="friends-icon" viewBox="0 0 16.00 16.00" fill="none"  stroke-linecap="round"
       stroke-linejoin="round" stroke-width="0.608">
       <g>
         <circle cx="8" cy="6" r="3.25" />
@@ -49,7 +49,3 @@
 
   </svg>
 </template>
-
-<script setup>
-import './style.scss';
-</script>
