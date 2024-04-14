4d214f450610890377861342644f2f81bfea2718
Максим <akaikinmax@mail.ru>
2024-04-14 16:36:05 +0700

    fix / убрал неиспользуемый код

--- server/servers/indexBase.js
+++ server/servers/indexBase.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userRouter = require('../routes/user.routes');
 const postRouter = require('../routes/post.routers');
-// const friendRouter = require('../routes/friend.router'); 
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
@@ -29,7 +28,6 @@ app.all('*', function (req, res, next) {
 app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
-// app.use('/api/friend', friendRouter);
 
 app.listen(PORT, () => {
   console.log(`base server is running on port ${PORT}`);
--- server/servers/indexSocket.js
+++ server/servers/indexSocket.js
@@ -4,12 +4,24 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const friendRouter = require('../routes/friend.router');
 const PORT = process.env.PORT || 5001;
-
+const baseURL = "*";
 const app = express();
 
 app.use(cors({
-  origin: "*"
+  baseURL,
+  credentials: true,
+  origin: true
 }));
+
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,token");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(express.json());
 app.use('/api/friend', friendRouter);
 
