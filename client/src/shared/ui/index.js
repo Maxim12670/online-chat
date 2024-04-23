a34542ccb6644bfbae5ed36ae78624294bb0819b
Максим <akaikinmax@mail.ru>
2024-04-23 17:13:05 +0700

    feat / реализован компонент аватара пользователей

--- client/src/shared/ui/index.js
+++ client/src/shared/ui/index.js
@@ -3,11 +3,13 @@ import MyButton from "./myButton/MyButton.vue";
 import ContentContainer from "./contentContainer/ContentContainer.vue";
 import SpriteSVG from "./spriteSVG/SpriteSVG.vue";
 import ModalWindow from "./modalWindow/ui/ModalWindow.vue";
+import UserAvatar from "./userAvatar/UserAvatar.vue";
 
 export {
   MyInput,
   MyButton,
   ContentContainer,
   SpriteSVG,
-  ModalWindow
+  ModalWindow,
+  UserAvatar
 }
---
+++ client/src/shared/ui/userAvatar/UserAvatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <img v-if="image === null" src="@/shared/assets/image/pict.jpg" class="avatar" alt="avatar">
+  <img v-else :src="`http://localhost:5000/${image}`" class="avatar" alt="avatar">
+</template>
+
+<script setup>
+import './style.scss';
+
+defineProps({
+  image: String
+});
+
+</script>
