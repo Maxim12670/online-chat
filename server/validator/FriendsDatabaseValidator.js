e988ad7145caac6e16e5f6aa6c9fd7959e69c623
Максим <akaikinmax@mail.ru>
2024-04-25 11:39:49 +0700

    refactor / изменил именования методов

--- server/validator/FriendsDatabaseValidator.js
+++ server/validator/FriendsDatabaseValidator.js
@@ -1,11 +1,14 @@
 const db = require('../db');
 
 class ValidatorFriend {
-  // проверка зарегистрирована пара или нет
-  async friendsPairExists(id_sender, id_recipient) {
+  // can add friend
+  async canAddFriend(id_sender, id_recipient) {
     try {
-      const result = await db.query(`SELECT id_sender FROM friends WHERE id_sender = ${id_recipient} AND id_recipient = ${id_sender}`)
-      console.log(result.rowCount)
+      const result = await db.query(
+        `SELECT id_sender 
+        FROM friends 
+        WHERE (id_sender = ${id_recipient} AND id_recipient = ${id_sender}`);
+      console.log('count', result.count)
       if (result.rowCount > 0) {
         return false;
       }
@@ -16,17 +19,22 @@ class ValidatorFriend {
       return false;
     }
   };
-  async OnlyPair(id_sender, id_recipient) {
+
+  async friendPairExists(id_sender, id_recipient) {
     try {
-      const result = await db.query('SELECT COUNT(*) FROM friends WHERE id_sender = $1 AND id_recipient = $2', [id_sender, id_recipient]);
-      if (result.rows[0].count > 0) {
+      const result = await db.query(
+        `SELECT COUNT(*) FROM friends
+        WHERE (id_sender = $1 AND id_recipient = $2 AND status = 'active')
+        OR (id_sender = $2 AND id_recipient = $1 AND status = 'active')`,
+        [id_sender, id_recipient]);
+      if (result.rows[0].count <= 0) {
         return false;
       }
       return true;
     } catch (error) {
       console.log('Произошла ошибка:', error);
     }
-  }
+  };
 }
 
 module.exports = new ValidatorFriend();
