a0a6ff7f494ab3fcf64e9037401ff6300eceec6a
Максим <akaikinmax@mail.ru>
2024-05-01 13:47:56 +0700

    feat / релаизован функционал получения и создания диалога

---
+++ client/src/api/dialogApiStore.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+const baseURLDialog = "http://localhost:5001/api/dialog";
+const apiRoutesDialog = {
+  createDialog: '/create',
+  getAllDialogs: '/dialogs'
+};
+
+export const useDialogApi = defineStore('dialogAPI', () => {
+  // создание нового диалога
+  const createDialog = async (idUser, idCompanion) => {
+    try {
+      await axios.post(`${baseURLDialog}${apiRoutesDialog.createDialog}`, {
+        idUser: idUser,
+        idCompanion: idCompanion
+      })
+        .catch(error => {
+          console.log('Что-то пошло не так...');
+        });
+    } catch (error) {
+      console.log('Произошла ошибка:', error);
+    }
+  };
+  // получить все диалоги пользователя
+  const getAllDialogs = async (idUser) => {
+    try {
+      const { data } = await axios.get(`${baseURLDialog}${apiRoutesDialog.getAllDialogs}`, {
+        params: {
+          "idUser": idUser
+        }
+      });
+      return data;
+    } catch (error) {
+      console.log('Произошла ошибка:', error);
+    }
+  }
+
+  return { createDialog, getAllDialogs }
+});
---
+++ client/src/stores/dialogStore.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import { useUserStore } from "./userStore";
+import { useDialogApi } from "@/api/dialogApiStore";
+
+export const useDialogStore = defineStore('dialogStore', () => {
+
+  const userStore = useUserStore();
+  const dialogAPI = useDialogApi();
+  const userId = userStore.userData.id;
+
+  // создать диалог
+  async function createDialog(idCompanion) {
+    try {
+      await dialogAPI.createDialog(userId, idCompanion);
+    } catch (error) {
+      console.log('Произошла ошибка:', error);
+    }
+  };
+  // получить все диалоги пользователя
+  async function getAllDialogs() {
+    try {
+      const result = await dialogAPI.getAllDialogs(userId);
+      return result;
+    } catch (error) {
+      console.log('Произошла ошибка:', error);
+    }
+  };
+
+  return { createDialog, getAllDialogs }
+});
