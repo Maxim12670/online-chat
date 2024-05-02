efe6072d31865a2b80e97aa4ea03dd1b6de6e8f6
Максим <akaikinmax@mail.ru>
2024-05-02 17:01:17 +0700

    feat / добавил лоадер в список пользователей

--- client/src/widgets/searchView/component/searchList/SearchList.vue
+++ client/src/widgets/searchView/component/searchList/SearchList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="search-list">
     <my-input class="search-list__input" v-model="filterString" placeholder="Поиск" type="text" :isRequired="false" />
-    <div class="search-list__container">
+    <loader-content v-if="!loadedPersons" class="search-list__loader"/>
+    <div v-else class="search-list__container">
       <not-data-stub v-if="!filterPersons.length" text="Нет данных"/>
       <search-item v-else class="search-list__item" v-for="(person, index) in filterPersons" :key="index" :id="person.id"
         :name="person.name" :surname="person.surname" :image="person.image"/>
@@ -12,13 +13,14 @@
 <script setup>
 import './style.scss';
 import { computed, onMounted, ref } from 'vue';
-import { MyInput, NotDataStub } from '@/shared/ui';
+import { MyInput, NotDataStub, LoaderContent } from '@/shared/ui';
 import SearchItem from '../searchItem/SearchItem.vue';
 import { useUserStore } from '@/stores/userStore.js';
 
 const userStore = useUserStore();
 const filterString = ref('');
 const persons = ref([]);
+const loadedPersons = ref(false)
 
 const filterPersons = computed(() => {
   if (filterString) {
@@ -30,6 +32,7 @@ const filterPersons = computed(() => {
 
 onMounted(async () => {
   persons.value = await userStore.getUsers(userStore.userData.id);
+  loadedPersons.value = true;
 })
 </script>
 
