4e6d082a3f805b5dab904b0edbf7e9aa1c96fb75
Максим <akaikinmax@mail.ru>
2024-05-02 15:55:58 +0700

    feat / добавил роутинг заглушки

--- client/src/shared/ui/index.js
+++ client/src/shared/ui/index.js
@@ -2,6 +2,7 @@ import MyInput from "./myInput/MyInput.vue";
 import MyButton from "./myButton/MyButton.vue";
 import ContentContainer from "./contentContainer/ContentContainer.vue";
 import SpriteSVG from "./spriteSVG/SpriteSVG.vue";
+import NotDataStub from "./notDataStub/ui/NotDataStub.vue";
 import ModalWindow from "./modalWindow/ui/ModalWindow.vue";
 import UserAvatar from "./userAvatar/UserAvatar.vue";
 
@@ -10,6 +11,7 @@ export {
   MyButton,
   ContentContainer,
   SpriteSVG,
+  NotDataStub,
   ModalWindow,
   UserAvatar
 }
