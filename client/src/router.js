2a6c58cd96a62ddbae10a7b5397acffbce471c69
Максим <akaikinmax@mail.ru>
2024-05-02 12:32:07 +0700

    faet / добавлен новый компонент и функция по получению сообщения диалога

--- client/src/router.js
+++ client/src/router.js
@@ -1,24 +1,39 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import MainPage from "./pages/mainPage/ui/MainPage.vue";
 import FormsPage from "./pages/formsPage/ui/FormsPage.vue";
-import { UserView, SearchView, MessageView, FriendsView } from "./widgets/index";
-
+import { UserView, SearchView, MessageView, FriendsView, ChatRoom } from "./widgets/index";
+import { useMessageStore } from "./stores/messageStore";
 
 
 const routers = [
   {
     name: 'MainPage', path: '/', component: MainPage,
     children: [
-      { path: 'user', component: UserView },
-      { path: 'search', component: SearchView },
+      { name: 'user', path: 'user', component: UserView },
+      { name: 'search', path: 'search', component: SearchView },
       {
-        path: 'dialogs', component: MessageView
+        name: 'dialogs', path: 'dialogs', component: MessageView
       },
-      { path: 'friends', component: FriendsView }
+      {
+        name: 'chat',
+        path: 'chat/:id', 
+        component: ChatRoom, 
+        props: (route) => ({
+          name: route.query.name,
+          surname: route.query.surname
+        }), 
+        beforeEnter: getDialogMessages },
+      { name: 'friends', path: 'friends', component: FriendsView }
     ]
   },
   { name: 'FormsPage', path: '/auth', component: FormsPage },
 ];
+
+async function getDialogMessages(to, from, next) {
+  const messageStore = useMessageStore();
+  to.params.messages = await messageStore.getAllMessageDialog(to.params.id);
+  next();
+}
 
 export default createRouter({
   history: createWebHashHistory(),
