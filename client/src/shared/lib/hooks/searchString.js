baeabf5e56691ea8ade20b5fa47bda904e72482d
Максим <akaikinmax@mail.ru>
2024-05-12 15:44:26 +0700

    feat / реализация метода поиска по строке в объекте person

--- client/src/shared/lib/hooks/searchString.js
+++ client/src/shared/lib/hooks/searchString.js
@@ -1,30 +1,13 @@
-import { ref, watch } from "vue";
 
-
-function searchString({ foo }) {
-  const query = ref('')
-  const timeoutId = ref(null)
-  const result = ref(null)
-  const isError = ref(false)
-
-  watch(query, () => {
-    if (timeoutId.value) clearTimeout(timeoutId.value)
-    timeoutId.value = setTimeout(async() => {
-        try {
-          result.value = await foo(query.value)
-          isError.value = false
-        } catch {
-          isError.value = true
-        }
-    }, 300)
-  })
-
-  return {
-    query,
-    result,
-    isError
-  }
-
+function searchByStringInPerson(str, array) {
+  console.log('filter string', str.value);
+  const regex = new RegExp(str, 'i');
+  const filteredArray = array.filter((item => {
+    if (regex.test(item.name) || regex.test(item.surname)) {
+      return item
+    }
+  }));
+  return filteredArray;
 }
 
-export { searchString };
+export { searchByStringInPerson };
