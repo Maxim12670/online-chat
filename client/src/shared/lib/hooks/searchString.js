afb1bc9624f2731b1121d81532a17a6ad692203a
Максим <akaikinmax@mail.ru>
2024-05-12 16:19:37 +0700

    feat / изменил параметры условия функции

--- client/src/shared/lib/hooks/searchString.js
+++ client/src/shared/lib/hooks/searchString.js
@@ -1,9 +1,10 @@
 
 function searchByStringInPerson(str, array) {
-  console.log('filter string', str.value);
+  console.log('filter string', str);
   const regex = new RegExp(str, 'i');
   const filteredArray = array.filter((item => {
-    if (regex.test(item.name) || regex.test(item.surname)) {
+    if (regex.test(item.name) || regex.test(item.surname)
+      || regex.test(item.name_companion) || regex.test(item.surname_companion)) {
       return item
     }
   }));
