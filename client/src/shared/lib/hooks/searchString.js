1f07b475ebbf5c936bd66d15d92d03e003de541a
Максим <akaikinmax@mail.ru>
2024-05-12 16:51:03 +0700

    error / fix bug

--- client/src/shared/lib/hooks/searchString.js
+++ client/src/shared/lib/hooks/searchString.js
@@ -1,14 +1,17 @@
-
+// массив может быть изначально пустым
+// жалуется на filter
 function searchByStringInPerson(str, array) {
   console.log('filter string', str);
   const regex = new RegExp(str, 'i');
-  const filteredArray = array.filter((item => {
-    if (regex.test(item.name) || regex.test(item.surname)
-      || regex.test(item.name_companion) || regex.test(item.surname_companion)) {
-      return item
-    }
-  }));
-  return filteredArray;
+  // if (array) {
+    const filteredArray = array.filter((item => {
+      if (regex.test(item.name) || regex.test(item.surname)
+        || regex.test(item.name_companion) || regex.test(item.surname_companion)) {
+        return item
+      }
+    }));
+    return filteredArray;
+  // }
 }
 
 export { searchByStringInPerson };
