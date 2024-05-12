44688fe904942bb0960025bd3b396a6d0be1778b
Максим <akaikinmax@mail.ru>
2024-05-12 15:44:37 +0700

    feat / изменил логику поиска пользователей

--- client/src/widgets/searchView/component/searchList/SearchList.vue
+++ client/src/widgets/searchView/component/searchList/SearchList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="search-list">
     <my-input class="search-list__input" v-model="filterString" placeholder="Поиск" type="text" :isRequired="false" />
-    <loader-content v-if="!loadedPersons" class="search-list__loader"/>
+    <loader-content v-if="!loadedPersons" class="search-list__loader" />
     <div v-else class="search-list__container">
-      <not-data-stub v-if="!filterPersons.length" text="Нет данных"/>
-      <search-item v-else class="search-list__item" v-for="(person, index) in filterPersons" :key="index" :id="person.id"
-        :name="person.name" :surname="person.surname" :image="person.image"/>
+      <not-data-stub v-if="!filterPersons.length" text="Нет данных" />
+      <search-item v-else class="search-list__item" v-for="(person, index) in filterPersons" :key="index"
+        :id="person.id" :name="person.name" :surname="person.surname" :image="person.image" />
     </div>
   </div>
 </template>
@@ -15,32 +15,20 @@ import './style.scss';
 import { computed, onMounted, ref } from 'vue';
 import { MyInput, NotDataStub, LoaderContent } from '@/shared/ui';
 import SearchItem from '../searchItem/SearchItem.vue';
+import { searchByStringInPerson } from '@/shared/lib/hooks/searchString';
 import { useUserStore } from '@/stores/userStore.js';
 
 const userStore = useUserStore();
 const filterString = ref('');
 const persons = ref([]);
-const loadedPersons = ref(false)
+const loadedPersons = ref(false);
 
 const filterPersons = computed(() => {
-  if (filterString) {
-    return persons.value.filter((person) =>
-      person.name.indexOf(filterString.value) !== -1 || person.surname.indexOf(filterString.value) !== -1)
-  }
-  return persons.value;
-})
+  return searchByStringInPerson(filterString.value, persons.value)
+});
 
 onMounted(async () => {
   persons.value = await userStore.getUsers(userStore.userData.id);
   loadedPersons.value = true;
 })
 </script>
-
-<!-- import { searchString } from '@/shared/lib/hooks/searchString';
-
-const { query: filterString, result: persons } = searchString({
-  foo: async (val) => {
-    console.log(val)
-    return await userStore.getUsers()
-  }
-}) -->
