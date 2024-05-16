637d86de89d60bc463e04181556d53452af7c48e
Максим <akaikinmax@mail.ru>
2024-05-16 11:39:35 +0700

    feat / добавил в токен возвраст пользователя

--- server/utils/generateAccessToken.js
+++ server/utils/generateAccessToken.js
@@ -7,6 +7,7 @@ const generateAccessToken = (user) => {
     email: user.email ? user.id : null,
     name: user.name ? user.name : null,
     surname: user.surname ? user.surname : null,
+    age: user.age ? user.age : null,
     city: user.city ? user.city : null,
     password: user.password ? user.password : null,
     image: user.image ? user.image : null
