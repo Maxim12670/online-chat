20731f2d307f076465eb31cd0145a5786aac00de
Максим <akaikinmax@mail.ru>
2024-05-21 10:25:19 +0700

    feat/ вынес переиспользуемый код

--- client/src/widgets/searchView/component/searchItem/SearchItem.vue
+++ client/src/widgets/searchView/component/searchItem/SearchItem.vue
@@ -1,29 +1,21 @@
 <template>
-  <div class="search-item">
-    <div class="search-item__photo">
-      <user-avatar :image="image" />
-    </div>
-    <div class="search-item__initials">
-      {{ `${surname} ${name}` }}
-    </div>
-    <div class="search-item__btns">
-      <button class="search-item__btn" @click="() => createDialog(id)">
-        <svg class="search-item__icon">
-          <use xlink:href="#message-icon"></use>
-        </svg>
-      </button>
-      <button class="search-item__btn" @click="sendFriendRequest">
-        <svg class="search-item__icon">
-          <use xlink:href="#friends-icon"></use>
-        </svg>
-      </button>
-    </div>
-  </div>
+  <card-item class="search-item" :name="name" :surname="surname" :image="image">
+    <button class="search-item__btn" @click="() => createDialog(id)">
+      <svg class="search-item__icon">
+        <use xlink:href="#message-icon"></use>
+      </svg>
+    </button>
+    <button class="search-item__btn" @click="sendFriendRequest">
+      <svg class="search-item__icon">
+        <use xlink:href="#friends-icon"></use>
+      </svg>
+    </button>
+  </card-item>
 </template>
 
 <script setup>
 import './style.scss';
-import { UserAvatar } from '@/shared/ui/index';
+import { CardItem } from '@/entities/ui';
 import { useFriendsStore } from '@/stores/friendsStore';
 import { useDialogStore } from '@/stores/dialogStore';
 
